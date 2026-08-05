e99ffa896e302e350e6631379e91d14430f532f7
saltyptato <36800046+saltyptato@users.noreply.github.com>
2026-08-04 20:38:46 -0600

    parameterized query

--- ShodanClient.py
+++ ShodanClient.py
@@ -6,6 +6,5 @@ class ShodanClient:
         self.api_key = api_key
         
     def basic_search(self, query):
-        search_url = requests.get(f"https://api.shodan.io/shodan/host/search?key={self.api_key}&query={query}").json()
-        ipList = [item["ip_str"] for item in search_url["matches"]] 
-        return ipList
+        search_results = requests.get(f"https://api.shodan.io/shodan/host/search?key={self.api_key}&query={query}").json()
+        return search_results
