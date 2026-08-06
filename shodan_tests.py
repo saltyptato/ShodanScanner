66fc43d686daafead35dc9541390ad0dfa04b5d7
saltyptato <36800046+saltyptato@users.noreply.github.com>
2026-08-05 18:54:40 -0600

    parameterized filter

--- shodan_tests.py
+++ shodan_tests.py
@@ -6,11 +6,12 @@ def main():
     SHODAN_API_KEY = os.getenv("SHODAN_API_KEY")
     client = ShodanClient(api_key=SHODAN_API_KEY)
 
-    product = input("What software do you want to scan? ")
-    search_results = ShodanClient.basic_search(client,f"product:{product}")
+    filter = input("Please provide a search filter from https://www.shodan.io/search/filters: ")
+    search_results = ShodanClient.basic_search(client,filter)
 
     for result in search_results["matches"]:
         print(f'IP: {result["ip_str"]}, Domain: {result["domains"]}, Product: {result["product"]}')
+        
 
 
 # defining main function
